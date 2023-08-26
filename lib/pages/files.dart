5daec0ac81cfdebc3a7e17fcc34fd3c5c29ebee8
Hari Prasad <haririo321@gmail.com>
2023-08-27 00:11:39 +0530

    FirebaseStorage works!

--- lib/pages/files.dart
+++ lib/pages/files.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path_provider/path_provider.dart';
@@ -20,42 +19,96 @@ class _FilesState extends State<Files> {
     futureFiles = FirebaseStorage.instance.ref('/files').listAll();
   }
 
+  Map<String, Color> fileColorMap = {
+    'jpg': Colors.blueAccent.shade100,
+    'png': Colors.blueAccent.shade100,
+    'jpeg': Colors.blueAccent.shade100,
+    'doc': Colors.blue.shade200,
+    'docx': Colors.blue.shade200,
+    'pdf': Colors.red.shade200,
+    'ppt': Colors.orange.shade200,
+    'xlsx': Colors.greenAccent.shade100
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: () {},
-            icon: const Icon(Icons.upload),
-            label: const Text('Upload')),
-        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-        body: FutureBuilder(
-          future: futureFiles,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final files = snapshot.data!.items;
-              return ListView.builder(
-                itemCount: files.length,
-                itemBuilder: (context, index) {
-                  final file = files[index];
-                  return ListTile(
-                    title: Text(file.name),
-                    trailing: IconButton(
-                        onPressed: () {
-                          downloadFile(file);
-                        },
-                        icon: const Icon(Icons.download)),
-                  );
-                },
-              );
-            } else if (snapshot.hasError) {
-              return const Center(child: Text('Error occured!'));
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ));
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          icon: const Icon(Icons.upload),
+          label: const Text('Upload')),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      body: FutureBuilder(
+        future: futureFiles,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final files = snapshot.data!.items;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  const Text('  All Files:'),
+                  const SizedBox(height: 10),
+                  ListView.builder(
+                    itemCount: files.length,
+                    reverse: true,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final file = files[index];
+                      final fileref =
+                          FirebaseStorage.instance.ref(file.fullPath);
+                      final Future<FullMetadata> fileMeta =
+                          fileref.getMetadata();
+
+                      return Card(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        child: ListTile(
+                          title: Row(children: [
+                            Card(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(6))),
+                                color: fileColorMap[
+                                    file.name.split('.').last.toLowerCase()],
+                                child: Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(8, 1, 8, 1),
+                                    child: Text(
+                                      file.name.split(".").last.toUpperCase(),
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
+                                    ))),
+                            Text(file.name.split('.').first)
+                          ]),
+                          subtitle: const Row(
+                            children: [Text('file time '), Text('file date')],
+                          ),
+                          trailing: IconButton(
+                              onPressed: () {
+                                downloadFile(file);
+                              },
+                              icon: const Icon(Icons.download)),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Could not retrieve data!'));
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 
   Future downloadFile(Reference ref) async {
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/secondary/file_type.dart';
 import 'package:netflix_clone/secondary/subjects.dart';
 import 'package:netflix_clone/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -41,41 +45,146 @@ List<Subjects> subjectList = [
 ];
 
 class HomeState extends State<Home> {
+  late Future<ListResult> futureFiles;
+  @override
+  void initState() {
+    super.initState();
+    futureFiles = FirebaseStorage.instance.ref('/files').listAll();
+  }
+
+  Map<String, Color> fileColorMap = {
+    'jpg': Colors.blueAccent.shade100,
+    'png': Colors.blueAccent.shade100,
+    'jpeg': Colors.blueAccent.shade100,
+    'doc': Colors.blue.shade200,
+    'docx': Colors.blue.shade200,
+    'pdf': Colors.red.shade200,
+    'ppt': Colors.orange.shade200,
+    'xlsx': Colors.greenAccent.shade100
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: [
-          const SizedBox(height: 10),
-          const Text('  Recently Uploaded:'),
-          const SizedBox(height: 10),
-          RecentlyAddedFileCard(
-            fileType: FileType.pdf,
-            title: 'Web Tech Notes',
-            uploaderStudent: 'Sruti',
-            uploadedTime: const TimeOfDay(hour: 12, minute: 45),
-          ),
-          RecentlyAddedFileCard(
-            title: 'C++ Important Questions',
-            fileType: FileType.ppt,
-            uploaderStudent: 'Abin',
-            uploadedTime: const TimeOfDay(hour: 10, minute: 23),
-          ),
-          RecentlyAddedFileCard(
-            title: 'DS Project Teams',
-            fileType: FileType.xlsx,
-            uploaderStudent: 'Karthik',
-            uploadedTime: const TimeOfDay(hour: 8, minute: 45),
-          ),
-          const SizedBox(height: 10),
-          const Text('  Your Subjects:'),
-          const SizedBox(height: 10),
-          for (Subjects a in subjectList) SubjectCard(subject: a)
-        ],
+      body: FutureBuilder(
+        future: futureFiles,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final files = snapshot.data!.items;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  const Text('  Recently Uploaded:'),
+                  const SizedBox(height: 10),
+                  ListView.builder(
+                    itemCount: 3,
+                    reverse: true,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final file = files[index];
+                      final fileref =
+                          FirebaseStorage.instance.ref(file.fullPath);
+                      final Future<FullMetadata> fileMeta =
+                          fileref.getMetadata();
+
+                      return Card(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        child: ListTile(
+                          title: Row(children: [
+                            Card(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(6))),
+                                color: fileColorMap[
+                                    file.name.split('.').last.toLowerCase()],
+                                child: Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(8, 1, 8, 1),
+                                    child: Text(
+                                      file.name.split(".").last.toUpperCase(),
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
+                                    ))),
+                            Text(file.name.split('.').first)
+                          ]),
+                          subtitle: const Row(
+                            children: [Text('file time '), Text('file date')],
+                          ),
+                          trailing: IconButton(
+                              onPressed: () {
+                                downloadFile(file);
+                              },
+                              icon: const Icon(Icons.download)),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('  Your Subjects:'),
+                  const SizedBox(height: 10),
+                  for (Subjects a in subjectList) SubjectCard(subject: a)
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Could not retrieve data!'));
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
-    ));
+      //     body: Padding(
+      //   padding: const EdgeInsets.all(10),
+      //   child: ListView(
+      //     children: [
+      //       const SizedBox(height: 10),
+      //       const Text('  Recently Uploaded:'),
+      //       const SizedBox(height: 10),
+      //       RecentlyAddedFileCard(
+      //         fileType: FileType.pdf,
+      //         title: 'Web Tech Notes',
+      //         uploaderStudent: 'Sruti',
+      //         uploadedTime: const TimeOfDay(hour: 12, minute: 45),
+      //       ),
+      //       RecentlyAddedFileCard(
+      //         title: 'C++ Important Questions',
+      //         fileType: FileType.ppt,
+      //         uploaderStudent: 'Abin',
+      //         uploadedTime: const TimeOfDay(hour: 10, minute: 23),
+      //       ),
+      //       RecentlyAddedFileCard(
+      //         title: 'DS Project Teams',
+      //         fileType: FileType.xlsx,
+      //         uploaderStudent: 'Karthik',
+      //         uploadedTime: const TimeOfDay(hour: 8, minute: 45),
+      //       ),
+      //       const SizedBox(height: 10),
+      //       const Text('  Your Subjects:'),
+      //       const SizedBox(height: 10),
+      //       for (Subjects a in subjectList) SubjectCard(subject: a)
+      //     ],
+      //   ),
+      // )
+    );
+  }
+
+  Future downloadFile(Reference ref) async {
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/${ref.name}');
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Downloading ${ref.name}')));
+
+    await ref.writeToFile(file);
+
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Downloaded ${ref.name}')));
   }
 }
 
